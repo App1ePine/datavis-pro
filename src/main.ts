a29b079234dfcf5d5fbafc762eec3ccc773595f1
ApplePine <djl@applepine.cn>
2026-01-30 20:10:38 +0800

    fix(ui): 恢复引入 index.css 修复样式缺失

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
+import './index.css';
 
 const app = createApp(App);
 const pinia = createPinia();
